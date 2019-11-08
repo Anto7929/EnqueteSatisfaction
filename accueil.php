a088788e48f66d853d13af906b9568f1d2f5005d
Anthony Nisol <nisola@saintjo.orggit config --global user.email nisola@saintjo.org>
2019-11-08 15:00:34 +0100

    Ajout styles div connexion

---
+++ accueil.php
@@ -0,0 +1,12 @@
+<head>
+    
+    <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    <?php include 'menu.php' ?>
+    
+</head>
+
+<body>
+    
+    dcdvf
+    
+</body>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<nav>   <!-- Menu de navigation -->
+            <ul>
+                <li><a href="statistiques.php">Statistiques</a></li>
+                <li><a href="gestion_enquete.php">Gérer les enquêtes</a></li>
+                <li><a href="deconnexion.php">Se déconnecter</a></li>
+            </ul>
+        </nav>
